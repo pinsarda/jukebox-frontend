9e21ee2c43a1ebe5dad2b1adff4eb2574cfa9285
Antoine Pinsard <apinsard@hackademint.org>
2025-02-26 16:25:54 +0100

    add proxy for cors policy errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,26 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    proxy: {
+    // Target is your backend API
+      '/api': {
+          target: 'https://jukebox.dev.hackademint.org', 
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          
+          configure: (proxy, options) => {
+             proxy.on('error', (err, _req, _res) => {
+              console.log('error', err);
+             });
+             proxy.on('proxyReq', (proxyReq, req, _res) => {
+              console.log('Request sent to target:', req.method, req.url);
+             });
+             proxy.on('proxyRes', (proxyRes, req, _res) => {
+              console.log('Response received from target:', proxyRes.statusCode, req.url);
+             });
+       },
+    },
+    },
+  },
 })
