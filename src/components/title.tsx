a43c3e5f87d1169b842012d805dc6a6213152ff8
Paolo Janvier <paolo.janvier@gmail.com>
2025-03-19 08:59:07 +0100

    correct/finish title component

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,22 +1,16 @@
-import {Card, CardBody, CardHeader, Image} from "@heroui/react";
+import {Card, Image} from "@heroui/react";
 
 export default function Title() {
   return (
-    <Card className="py-4 w-full h-12">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <Image
-            alt="Card background"
-            className="object-cover rounded-xl"
-            src="https://heroui.com/images/hero-card-complete.jpeg"
-            width={25}
-            height={25}
-        />
-      </CardHeader>
-      <CardBody className="overflow-visible py-2">
-        <h4 className="font-bold text-large">Frontend Radio</h4>
-        <p className="text-tiny uppercase font-bold">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small>
-      </CardBody>
+    <Card className="py-4 w-full h-12 flex-row pb-0 pt-2 px-4 items-start justify-items-center space-x-3">
+      <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src="https://heroui.com/images/hero-card-complete.jpeg"
+          width={32.5}
+          height={32.5}
+      />
+      <h4 className="font-bold text-large">Frontend Radio</h4>
     </Card>
   );
 }
