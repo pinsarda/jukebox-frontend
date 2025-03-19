4f5e3d64ababa23bfff0cf86a01b224122e63d4c
Paolo Janvier <paolo.janvier@gmail.com>
2025-03-19 15:47:04 +0100

    make album and artist links in music card

--- src/components/music-card.tsx
+++ src/components/music-card.tsx
@@ -1,6 +1,6 @@
 import { add_to_queue } from "@/api-wrapper";
 import { Music } from "@/types/musics";
-import {Card, Image} from "@heroui/react";
+import {Card, Image, Link} from "@heroui/react";
 
 interface MusicCardProps {
   music: Music,
@@ -28,9 +28,9 @@ export default function MusicCard({ music }: MusicCardProps) {
       />
       <div>
         <h4 className="font-bold text-large text-start">{ music.title }</h4>
-        <h4 className="text-medium text-start">{ music.artists[0].name }</h4>
+        <Link className="text-medium text-start w-full" underline="hover" color="foreground" href={"/artist/" + music.artists[0].id }>{ music.artists[0].name }</Link>
       </div>
-      <h4 className="ml-auto text-medium">{ music.album_title }</h4>
+      <Link className="ml-auto text-medium" underline="hover" color="foreground" href={"/album/" + music.album_id }>{ music.album_title }</Link>
       <h4 className="text-medium">1:00</h4>
     </Card>
   );
