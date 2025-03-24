cc7928d6c4af0d076aac751d37cb4be8d8b3fd65
Antoine Pinsard <apinsard@hackademint.org>
2025-03-24 01:44:23 +0100

    add loading indicator for enqueing music from db

--- src/components/music-card.tsx
+++ src/components/music-card.tsx
@@ -1,6 +1,7 @@
 import { add_to_queue } from "@/api-wrapper";
 import { Music } from "@/types/backend";
-import {Card, Image, Link} from "@heroui/react";
+import {Card, Image, Link, Spinner} from "@heroui/react";
+import { useQuery } from "@tanstack/react-query";
 
 interface MusicCardProps {
   music: Music,
@@ -8,7 +9,18 @@ interface MusicCardProps {
 
 export default function MusicCard({ music }: MusicCardProps) {
 
-  
+  const { isLoading, data, isError, refetch } = useQuery({
+    queryKey: ['addtoqueue_' + (music.id)],
+    enabled: false,
+    refetchInterval: false,
+    retry: false,
+    queryFn: () =>
+      add_to_queue({ id : music.id })
+      .then((res) => {
+          res.json()
+        }
+      ),
+  });
 
   return (
     <Card className="flex-row p-4 items-center justify-items-center gap-4"
@@ -17,7 +29,9 @@ export default function MusicCard({ music }: MusicCardProps) {
       isHoverable={true}
       disableRipple={true}
       onPress={() => {
-        add_to_queue({ id : music.id });
+        if (!isLoading) {
+          refetch()
+        }
       }}>
       <Image
           alt="Card background"
@@ -30,6 +44,9 @@ export default function MusicCard({ music }: MusicCardProps) {
         <h4 className="font-bold text-large text-start">{ music.title }</h4>
         <Link className="text-medium text-start w-full" underline="hover" color="foreground" href={"/artist/" + music.artists[0].id }>{ music.artists[0].name }</Link>
       </div>
+      {isLoading &&
+        <Spinner></Spinner>
+      }
       <Link className="ml-auto text-medium" underline="hover" color="foreground" href={"/album/" + music.album_id }>{ music.album_title }</Link>
       <h4 className="text-medium">1:00</h4>
     </Card>
